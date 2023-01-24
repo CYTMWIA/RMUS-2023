f66fde3b022e52978e9a4dde00f0d84e5533356e
CYTMWIA <cytmwia@gmail.com>
2023-01-24 23:53:23 +0800

    show target blocks

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker exec -it client /opt/ros/noetic/env.sh /opt/workspace/devel_isolated/env.sh /opt/ep_ws/devel/env.sh rostopic pub -1 /reset geometry_msgs/Point "{x: 0.0, y: 0.0, z: 0.0}"
+
+docker exec -it client /opt/ros/noetic/env.sh /opt/workspace/devel_isolated/env.sh /opt/ep_ws/devel/env.sh rostopic echo -n 1 /judgement/exchange_markers
